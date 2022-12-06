f2f2baa7fd9157b29f11e410dee9be8137c66b8f
Chris Shea <chris.shea@synd.io>
2022-12-06 18:15:44 -0500

    Document how we find the position

--- src/day6.rs
+++ src/day6.rs
@@ -1,6 +1,17 @@
 use aoc_runner_derive::aoc;
 use std::collections::VecDeque;
 
+/// Find the position in the datastream after a marker of length
+/// `marker_size` of distinct characters.
+///
+/// How does it work? We create a buffer to hold a window of
+/// characters as we go through stream. For each character we pull of
+/// the stream, we check to see if it's in the current buffer. If it
+/// is, we remove characters off the front of the buffer until we pull
+/// of its duplicate. If it's not in the buffer and the buffer is
+/// full, then we're at the end of the marker, and the next character
+/// is the position we're looking for. If we need to keep going, we
+/// push the character onto the buffer and move ahead.
 fn find_start(stream: &str, marker_length: usize) -> usize {
     let mut buf = VecDeque::with_capacity(marker_length);
     for (i, c) in stream.chars().enumerate() {
